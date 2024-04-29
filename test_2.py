1fb1551651ab2cd7074b6716731d1c85f5420966
apignato <168423723+apignato@users.noreply.github.com>
2024-04-29 16:58:12 +0200

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -41,7 +41,7 @@ end_date = datetime.strptime(df.Date_bis.max(), '%Y-%m-%d')
 list_date_image_str = []
 list_date_image_dt = []
 for file in os.listdir(f"stations/{option}/images"):
-   date_image_str = file[6:-4]
+   date_image_str = str(file[6:-4])
    date_image_dt = datetime.strptime(date_image_str,"%Y-%m-%d")
    list_date_image_str.append(date_image_str)
    list_date_image_dt.append(date_image_dt)
