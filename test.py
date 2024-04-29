1bfa096f9b4d92c001fecc0d9e9cb55e95c159d0
apignato <168423723+apignato@users.noreply.github.com>
2024-04-29 12:25:19 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from datetime import datetime
+
+import os
+import matplotlib.pyplot as plt
+
+
+# Create a Streamlit app
+
+option = st.selectbox(
+   "",
+   ("La Durance à Meyrargues", "La Durance à Saint-Paul-les-Durance", "xxxx"),
+   index=None,
+   placeholder="Selectionner une station...",
+)
+
+option_formatted = str(option).lower().replace(" ","_")
+
+
+st.title(f'{option}')
+
+########## Creation du slider #############
+
+# Create a slider to control the position of the vertical line
+
+start_date = datetime.strptime('2015 01 01', '%Y %m %d')
+end_date = datetime.strptime('2024 04 16', '%Y %m %d')
+
+selected_date = st.slider('', min_value=start_date, max_value=end_date, value=start_date)
+
+# Créer une entrée numérique où l'utilisateur peut saisir une valeur
+#input_date_str = st.text_input("Ou sélectionner une date en input (YYYY-MM-DD)", "2024-04-16")
+#input_date = datetime.strptime(input_date_str, '%Y-%m-%d')
+#d = st.date_input("sélectionner une date en input", datetime.date(2015, 1, 1))
+
+
+#if input_date:
+    #selected_date = input_date
+#st.slider('Selectionner une date avec le curseur', min_value=start_date, max_value=end_date, value=selected_date)
+
+########### Plot de la courbe de debit ##############
+
+df = pd.read_csv(f'/Users/apollinepignato/Documents/Streamlit_project/{option}/debit_{option_formatted}.csv')
+df = df.rename(columns={'Date (TU)': 'Date', 'Valeur (en l/s)': 'Valeur'})
+df['Date'] = pd.to_datetime(df['Date']) 
+
+df['Date_bis'] = df['Date'].dt.strftime("%Y-%m-%d")
+
+
+fig = px.line(df, x='Date_bis', y='Valeur', labels={'Date_bis': 'Temps', 'Valeur': 'Debit (en L/s)'}, title="Debit d'eau")
+
+
+
+############
+
+fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                    dict(count=6,
+                     label="6m",
+                     step="month",
+                     stepmode="backward"),
+                    dict(count=1,
+                     label="YTD",
+                     step="year",
+                     stepmode="todate"),
+                    dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                    dict(step="all")
+            ])
+        ),
+            rangeslider=dict(
+                visible=True
+        ),
+            type="date"
+    )
+)
+
+
+# Add vertical line
+fig.add_vline(x=selected_date, line_dash="dash", line_color="red")
+
+##########
+
+# Display the plot
+st.plotly_chart(fig)
+
+
+########### Images satellite ##############
+
+# Formater la date pour l'affichage dans le titre
+formatted_date = selected_date.strftime('%Y-%m-%d')  # Formatage de la date (par exemple "26/04/2024")
+
+# Afficher le titre avec la date sélectionnée
+
+#st.title (f'Image satellite du {formatted_date}')
+
+st.markdown(f'**Image satellite du {formatted_date}**')
+    
+chemin_image = f'{option}/image_{formatted_date}.png'  # Remplacez "chemin_vers_images" par le chemin de votre dossier contenant les images
+if os.path.exists(chemin_image):
+    image = plt.imread(chemin_image)
+    st.image(image, caption=f'Image_{formatted_date}', use_column_width=True)
+else:
+    st.write('Pas d\'image satellite disponible a cette date.')
+
